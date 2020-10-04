790efced9688b35652e2c518fe75a217c7b63b4a
will <williamspalding00@gmail.com>
2020-10-03 20:43:02 -0700

    created home page

---
+++ TribeApp/routes.py
@@ -0,0 +1,10 @@
+
+from flask import render_template
+
+from TribeApp import app
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html', title='Welcome')
---
+++ tribe.py
@@ -0,0 +1,1 @@
+from TribeApp import app
