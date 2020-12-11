9b0eceecf4b8014e3639353c6d0759cdc5f8afb4
will <williamspalding00@gmail.com>
2020-12-10 16:46:21 -0800

    added tribe-user relationship

--- TribeApp/Models/UserModel.py
+++ TribeApp/Models/UserModel.py
@@ -1,8 +1,14 @@
 from TribeApp import db
+
 from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy import Table, Column, Integer, ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+
 import uuid
 
 class User(db.Model):
+    __tablename__ = 'User'
     id = db.Column(UUID(as_uuid=True), 
                         primary_key=True, 
                         default=uuid.uuid4, 
@@ -11,3 +17,5 @@ class User(db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    tribe_id = Column(UUID(as_uuid=True), ForeignKey('Tribe.id'))
+    tribe = relationship('Tribe')
