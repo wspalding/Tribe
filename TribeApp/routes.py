aa169777e3889ca4751751866a47a8248b66dc15
will <williamspalding00@gmail.com>
2020-12-10 22:37:19 -0800

    adding nav partial

--- TribeApp/routes.py
+++ TribeApp/routes.py
@@ -1,5 +1,6 @@
 
 from flask import render_template
+from flask import jsonify
 from TribeApp import app
 
 
@@ -7,3 +8,15 @@ from TribeApp import app
 @app.route('/index')
 def index():
     return render_template('index.html', username='person1')
+
+
+
+
+
+@app.route('/' methods=['GET'])
+def test_api()
+    response = {
+        "value1": "hello",
+        "value2": "world"
+    }
+    return jsonify(response)
--- TribeApp/static/scripts/MainNavigation.js
+++ TribeApp/static/scripts/MainNavigation.js
@@ -3,7 +3,13 @@ $( document ).ready(function() {
 });
 
 function initLoginButton(){
-    $('#login-button').on('click', function(){
-        console.log("buttoon clicked");
+    $('#login-button').on('click', function(event){
+        $('#login-modal').style.display = "block";
+    })
+    // When the user clicks anywhere outside of the modal, close it
+    $(window).on('click', function(event) {
+        if (event.target == $('#login-button')) {
+            $('#login-modal').style.display = "none";
+        }
     })
 }
